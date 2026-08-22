ca3580890244b276a505516c1aa824a0dba4386f
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-08-22 18:42:55 +0545

    chor: added path maker and checker in same function that creates LOG

--- src/koushin/logger_config.py
+++ src/koushin/logger_config.py
@@ -1,22 +1,31 @@
 import logging
 
+from pathlib import Path
+
+import os 
 
 def setup_logger(name:str):
-    from koushin.config.config import cretate_logger_file
+    # from config.config import cretate_logger_file
     """
-       This function will set up logger 
+     It will create a LOG dir under root project 
+     make sure to add it in .gitignore
+    
+    This function will set up logger 
        
        ARGS:
        
         name -> logger name  
 
-    """
 
+    """
+    current_path = Path.cwd()
+    log_path = current_path / "LOG"
+    os.makedirs(log_path,exist_ok=True)
     logger = logging.getLogger(name)
     formatter = logging.Formatter(
          "| %(asctime)s | %(levelname)s | %(name)s | %(message)s |"
     )
-    file_path = cretate_logger_file()
+    file_path = log_path
     file_handler = logging.FileHandler(file_path/"koushin.log")
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
